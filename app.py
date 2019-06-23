aacd9c9b80e232f338fb4b7834b67d392fd3fa8c
Wilson <wilsonliao@iii.org.tw>
2019-06-24 06:12:23 +0800

    Fix bug for calculate standard weight

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ def standard():
     height = int(request.form.get('height'))
     weight = int(request.form.get('weight'))
     std_h_w = height - 105
-    if weight >= 40:
+    if std_h_w >= 40:
         comment = "重度肥胖"
-    elif weight >= 35 and weight < 40:
+    elif std_h_w >= 35 and std_h_w < 40:
         comment = "中度肥胖"
-    elif weight >= 30 and weight < 35:
+    elif std_h_w >= 30 and std_h_w < 35:
         comment = "輕度肥胖"
-    elif weight >= 25 and weight < 30:
+    elif std_h_w >= 25 and std_h_w < 30:
         comment = "超重"
-    elif weight >= 18 and weight < 25:
+    elif std_h_w >= 18 and std_h_w < 25:
         comment = "正常"
-    elif weight < 18:
+    elif std_h_w < 18:
         comment = "過輕"
     return render_template(
         'index.html', comment=comment, weight=weight, height=height
